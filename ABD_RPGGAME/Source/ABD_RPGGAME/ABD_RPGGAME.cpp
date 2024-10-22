dc379c5365c5d1d70e7e03da404f9306befc9e9e
BDAKOREA98 <nani1541@gmail.com>
2024-10-21 19:37:21 -0700

    Asset

---
+++ ABD_RPGGAME/Source/ABD_RPGGAME/ABD_RPGGAME.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ABD_RPGGAME.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ABD_RPGGAME, "ABD_RPGGAME" );
