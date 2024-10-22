0e1b8d4ac8a29a43d0777735cd55cb0970f1e9e0
BDAKOREA98 <nani1541@gmail.com>
2024-10-21 19:22:15 -0700

    Test

--- Source/ABD_RPGGAME/ABD_RPGGAME.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ABD_RPGGAME.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ABD_RPGGAME, "ABD_RPGGAME" );
